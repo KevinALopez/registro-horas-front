7459689192151463643c8ef817440405353002db
jonathanlc96 <jony9612@gmail.com>
2025-02-18 11:39:34 +0100

    creación de primeros componentes,home,login,profile y algunas rutas. Instalación de bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: "", pathMatch: 'full', redirectTo: 'login' },
+    { path: "login", component: LoginComponent },
+    { path: "home", component: HomeComponent },
+    { path: "**", redirectTo: 'login' }
+
+
+
+];
