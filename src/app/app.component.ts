d1a26a8a7a909e3d3a9ee2eb58a7310aad6c2c0d
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-18 18:08:50 +0100

    header y footer en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from "./component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
