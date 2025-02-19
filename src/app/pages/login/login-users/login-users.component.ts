09f502a68c689147ef442f584375e6da6ba1d4be
Kevin Batista <kevinbatistad@gmail.com>
2025-02-20 00:15:05 +0100

    se corrigen los fallos al hacer merge (SOURCETREE DEJAME MORIR EN PAZ)

--- src/app/pages/login/login-users/login-users.component.ts
+++ src/app/pages/login/login-users/login-users.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { UsersService } from './../../../services/users.service';
+import Swal from 'sweetalert2';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-users',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-users.component.html',
   styleUrl: './login-users.component.css'
 })
 export class LoginUsersComponent {
+  loginForm: FormGroup
+  UsersService = inject(UsersService);
+  router = inject(Router)
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    })
+  }
+  async onSubmit() {
+    try {
+      const response = await this.UsersService.login(this.loginForm.value);
+      Swal.fire('login', 'login Correcto', 'success')
+      localStorage.setItem('store_token', response.message);
+      this.router.navigateByUrl('/home');
 
+    } catch (error) {
+      Swal.fire('login', 'Usuario y/o Contrasena incorrecta', 'error');
+    }
+
+  }
 }
