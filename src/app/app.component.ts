1e4a7ae0ea3d013ba9ac8af28d60ccea461ce6e2
Kevin Batista <kevinbatistad@gmail.com>
2025-02-19 12:52:12 +0100

    correccion error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
-import { TestingComponent } from './testing/testing.component';
+
 
 @Component({
   selector: 'app-root',
