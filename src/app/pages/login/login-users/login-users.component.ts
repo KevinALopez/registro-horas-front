36c256105fa58e005f68f5b366cf2c433d752774
Kevin Batista <kevinbatistad@gmail.com>
2025-02-19 12:41:12 +0100

    se empieza el ts del login

--- src/app/pages/login/login-users/login-users.component.ts
+++ src/app/pages/login/login-users/login-users.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-users',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-users.component.html',
   styleUrl: './login-users.component.css'
 })
 export class LoginUsersComponent {
+  loginForm: FormGroup
 
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      usarname: new FormControl("", []),
+      password: new FormControl("", [])
+    }, [])
+  }
+  async getLogin() {
+
+  }
 }
