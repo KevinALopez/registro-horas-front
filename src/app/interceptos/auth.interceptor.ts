77ffca2e7f670b3ff963b3ad9c085c96dcbd0849
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-20 19:31:19 +0100

    actualice el token para que me funcionara

--- src/app/interceptos/auth.interceptor.ts
+++ src/app/interceptos/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   // For testing
 
-  const token = "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc0MDA1OTk0OH0.r7tzmrEsuRptKUKKy7Ktml50Hok42QeNrE7nJ2RQ7d4";
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc0MDA3NTI2OX0.MCu6G2z_Hq8NfCutlfDJmA_aEHDZngKvtIbvrSfG4xE";
   if (token) {
     const reqToken = req.clone({
       setHeaders: {
