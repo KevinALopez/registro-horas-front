133178c6c598b2cc5fdbcc2e1403a6cf14bb73d2
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-20 18:45:12 +0100

    implement filters by name and status on projects list

--- src/app/pages/projects-list/projects-list.component.ts
+++ src/app/pages/projects-list/projects-list.component.ts
@@ -3,10 +3,14 @@ import { ProjectComponent } from './project/project.component';
 import { ProjectsService } from '../../services/projects.service';
 import { IProject } from '../../interfaces/iproject';
 import Swal from 'sweetalert2';
+import {
+  filters,
+  ListFiltersComponent,
+} from './list-filters/list-filters.component';
 
 @Component({
   selector: 'app-projects-list',
-  imports: [ProjectComponent],
+  imports: [ProjectComponent, ListFiltersComponent],
   templateUrl: './projects-list.component.html',
   styleUrl: './projects-list.component.css',
 })
@@ -30,6 +34,30 @@ export class ProjectsListComponent {
       const { data } = await this.projectsService.getAll();
 
       this.projects = data;
+    } catch ({ message }: any) {
+      Swal.fire('Error al obtener los proyectos', message, 'error');
+    }
+  }
+
+  async filterProjects(filter: filters) {
+    try {
+      const { data } = await this.projectsService.getAll();
+
+      if (filter.status === 'all') {
+        this.projects = data.filter((project) => {
+          return project.name.toLowerCase().includes(filter.name.toLowerCase());
+        });
+
+        return;
+      }
+
+      this.projects = data
+        .filter((project) => {
+          return project.name.toLowerCase().includes(filter.name.toLowerCase());
+        })
+        .filter((project) => {
+          return project.status === filter.status;
+        });
     } catch ({ message }: any) {
       Swal.fire('Error al obtener los proyectos', message, 'error');
     }
