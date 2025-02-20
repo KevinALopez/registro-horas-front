e86f34b0dd5ca3b955c1646c5b53e380564659fe
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-20 13:59:12 +0100

    implement deleteProject on project component

--- src/app/pages/projects-list/project/project.component.ts
+++ src/app/pages/projects-list/project/project.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { IProject } from '../../../interfaces/iproject';
+import Swal from 'sweetalert2';
+import { ProjectsService } from '../../../services/projects.service';
 
 @Component({
   selector: 'app-project',
@@ -7,14 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './project.component.css',
 })
 export class ProjectComponent {
-  project = {
-    id: 6,
-    name: 'Project F',
-    description: 'Security Audit',
-    start: '2024-06-20T07:15:00.000Z',
-    end: '2024-12-20T17:15:00.000Z',
-    status: 'cancel',
-    estimated_hours: '100.00',
-    worked_hours: '10.00',
-  };
+  @Input() project!: IProject;
+
+  projectService = inject(ProjectsService);
+
+  async deleteProject(projectId: number) {
+    try {
+      const userInput = await Swal.fire({
+        title: '¿Eliminar este proyecto?',
+        text: 'Esta accion eliminara el proyecto de manera permanente. ¿Estas seguro de querer continuar?',
+        icon: 'warning',
+        reverseButtons: true,
+        confirmButtonText: 'Eliminar',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+      });
+
+      if (!userInput.isConfirmed) return;
+
+      const deleteResponse = await this.projectService.deleteById(projectId);
+
+      Swal.fire(
+        'Eliminacion de un proyecto',
+        'El proyecto fue eliminado con exito.',
+        'success'
+      );
+    } catch ({ message }: any) {
+      Swal.fire('Eliminacion de un proyecto', message, 'error');
+    }
+  }
 }
