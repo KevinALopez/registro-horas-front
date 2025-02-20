ddab496b38ad3f7353a3e060490226060c7a0bf0
jonathanlc96 <jony9612@gmail.com>
2025-02-20 14:12:29 +0100

    fomulario de editProject v1. Formulario responsive y primer intento de llamar al back para que se llenen los campos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { EditUserComponent } from './pages/edit-user/edit-user.component';
+import { EditProjectComponent } from './pages/edit-project/edit-project.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  {
-    path: 'edit-user', component: EditUserComponent
-  },
+  { path: 'edit-user', component: EditUserComponent },
+  { path: 'projects/edit-project/:projectId', component: EditProjectComponent },
   { path: '**', redirectTo: 'login' },
 ];
