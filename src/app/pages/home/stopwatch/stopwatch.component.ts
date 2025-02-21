2bde7a1ec53cf2ca447489fa413cc7b7af5d9855
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-21 21:07:49 +0100

    bug fix on stopwatch

--- src/app/pages/home/stopwatch/stopwatch.component.ts
+++ src/app/pages/home/stopwatch/stopwatch.component.ts
@@ -14,13 +14,20 @@ export class StopwatchComponent {
   formattedTime: string = '00:00:00';
   running: boolean = false;
   startText: string = 'Comenzar Jornada';
-  pauseText: string = 'Iniciar Pausa';
   timerRef: any;
-
-  hoursService = inject(HoursService);
   startId: number = -1;
 
-  async startTimer($event: any) {
+  counterPause!: number;
+  formattedTimePause: string = '00:00:00';
+  runningPause: boolean = false;
+  pauseText: string = 'Iniciar Pausa';
+  timerRefPause: any;
+  startIdPause: number = -1;
+  canPause: boolean = false;
+
+  hoursService = inject(HoursService);
+
+  async startTimer() {
     if (this.running) return this.logHours();
 
     const startTime = Date.now() - (this.counter || 0);
@@ -31,14 +38,18 @@ export class StopwatchComponent {
         'yyyy-MM-dd HH:mm:ss',
         'en-US'
       );
-      const { id } = await this.hoursService.registerStart(formattedStarTime);
-      this.startId = id;
+
+      if (!this.runningPause) {
+        const { id } = await this.hoursService.registerStart(formattedStarTime);
+        this.startId = id;
+      }
     } catch ({ message }: any) {
       Swal.fire('Error', message, 'error');
       return;
     }
 
     this.running = !this.running;
+    this.canPause = !this.canPause;
     this.startText = 'Finalizar Jornada';
 
     this.timerRef = setInterval(() => {
@@ -48,7 +59,39 @@ export class StopwatchComponent {
     }, 1000);
   }
 
-  startPause() {}
+  async startPause() {
+    if (this.runningPause) return this.logPauseHours();
+
+    const startTime = Date.now() - (this.counterPause || 0);
+
+    try {
+      const formattedStarTime = formatDate(
+        startTime,
+        'yyyy-MM-dd HH:mm:ss',
+        'en-US'
+      );
+      const { id } = await this.hoursService.registerPauseStart(
+        formattedStarTime,
+        'break'
+      );
+      this.startIdPause = id;
+    } catch ({ message }: any) {
+      Swal.fire('Error', message, 'error');
+      return;
+    }
+
+    clearInterval(this.timerRef);
+    this.running = !this.running;
+
+    this.runningPause = !this.runningPause;
+    this.pauseText = 'Finalizar Pausa';
+
+    this.timerRefPause = setInterval(() => {
+      this.counterPause = Date.now() - startTime;
+      const elapsedTime = new Date(0, 0, 0, 0, 0, 0, this.counterPause);
+      this.formattedTimePause = formatDate(elapsedTime, 'HH:mm:ss', 'en-US');
+    }, 1000);
+  }
 
   logHours() {
     try {
@@ -65,14 +108,40 @@ export class StopwatchComponent {
     }
 
     this.running = !this.running;
+    this.canPause = false;
     this.formattedTime = '00:00:00';
     clearInterval(this.timerRef);
+    this.counter = 0;
     this.startText = 'Comenzar Jornada';
 
     Swal.fire('Jornada finalizada', 'Gracias por tu trabajo', 'success');
   }
 
+  logPauseHours() {
+    try {
+      const formattedEndTime = formatDate(
+        Date.now(),
+        'yyyy-MM-dd HH:mm:ss',
+        'en-US'
+      );
+      this.hoursService.registerPauseEnd(formattedEndTime, this.startIdPause);
+      this.startIdPause = -1;
+    } catch ({ message }: any) {
+      Swal.fire('Error', message, 'error');
+      return;
+    }
+
+    this.startTimer();
+
+    this.runningPause = !this.runningPause;
+    this.formattedTimePause = '00:00:00';
+    this.pauseText = 'Iniciar Pausa';
+    clearInterval(this.timerRefPause);
+    this.counterPause = 0;
+  }
+
   ngOnDestroy() {
     clearInterval(this.timerRef);
+    clearInterval(this.timerRefPause);
   }
 }
