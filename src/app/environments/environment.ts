935a01f1678d4cf8b5b1f38cdaacc8747ee3f1ae
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-21 10:15:20 +0100

    tokenName: 'store_token',

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     apiUrl: 'http://localhost:3000/api',
-    tokenName:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInJvbGUiOiJhZG1pbiIsImlhdCI6MTczOTk2MzgxN30.aXIexD2A0CqdIVVd548y_H45vZog6mXJiXl06mLZbEM'
+    tokenName: 'store_token',
 };
